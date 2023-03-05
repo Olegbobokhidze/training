af8c5a6dc6ea6d43cdb99c5d0ef6228c46e1c2b6
Oleg Bobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2023-03-05 17:15:24 +0400

    only numbers

--- script.js
+++ script.js
@@ -3,5 +3,5 @@
 // In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
 
 const filterFunct = (arr) => {
-  return arr.filter((val) => val !== NaN);
+  return arr.filter((e) => Number.isInteger(e));
 };
